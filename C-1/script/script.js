a6405b765ff78480537243f62de57d5808da5c53
JoyLeeA <dlwhdgk8@gmail.com>
2021-08-16 02:25:11 +0900

    init

---
+++ C-1/script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".nav > ul > li").mouseover(function(){
+       $(this).find(".submenu").stop().slideDown(200);
+    });
+    
+    $(".nav > ul > li").mouseleave(function(){
+       $(this).find(".submenu").stop().slideUp(200);        
+    });
+});
