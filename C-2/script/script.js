89a6c5b418524f17770fb307054f41967363b488
JoyLee <dlwhdgk8@gmail.com>
2021-08-31 08:41:16 +0900

    C3

--- C-2/script/script.js
+++ C-2/script/script.js
@@ -7,4 +7,26 @@ $(document).ready(function(){
        $(this).find(".submenu").stop().slideUp(200);
     });
     
+    //Slide
+    var currentIndex = 0;
+    var slideCount = $(".slideImg").length;
+    
+    setInterval(function(){
+        var nextIndex = (currentIndex + 1) % slideCount;
+        $(".slideList").find(".slideImg").eq(currentIndex).stop().fadeOut();
+        $(".slideList").find(".slideImg").eq(nextIndex).stop().fadeIn();
+        currentIndex = nextIndex;    
+    }, 3000);
+    
+    //layer
+    $(".popup").click(function(e){
+        e.preventDefault;
+        $(".layer-bg").css("display", "block");
+        $(".layer").css("display", "block");
+    });
+    $(".close").click(function(e){
+        e.preventDefault;
+        $(".layer-bg").css("display", "none");
+        $(".layer").css("display", "none");
+    });    
 });
---
+++ C-3/script/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    
+    //GNB
+   $(".nav > ul > li").mouseover(function(){
+      $(this).find(".submenu").stop().slideDown(200); 
+   }); 
+   $(".nav > ul > li").mouseleave(function(){
+      $(this).find(".submenu").stop().slideUp(200); 
+   });  
+    
+    //slide
+    var currentIndex = 0;
+    var slideCount = $(".slideImg").length;
+    
+    setInterval(function(){
+        var nextIndex = (currentIndex + 1) % slideCount ;
+        $(".slideList").find(".slideImg").eq(currentIndex).stop().fadeOut();
+        $(".slideList").find(".slideImg").eq(nextIndex).stop().fadeIn();
+        currentIndex = nextIndex;
+    }, 3000);
+    
+    //layer
+    $(".popup").click(function(e){
+        e.preventDefault;
+        $(".layer-bg").css("display", "block");
+        $(".layer").css("display", "block");
+    });
+    $(".close").click(function(e){
+        e.preventDefault;
+        $(".layer-bg").css("display", "none");
+        $(".layer").css("display", "none");
+    });
+});
