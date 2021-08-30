166f111a1c2b871dcc6ce3c815eabccecb5dd826
JoyLeeA <dlwhdgk8@gmail.com>
2021-08-30 14:18:26 +0900

    C2

---
+++ C-2/script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   //GNB
+    $(".nav > ul > li").mouseover(function(){
+       $(this).find(".submenu").stop().slideDown(200);
+    });
+    $(".nav > ul > li").mouseleave(function(){
+       $(this).find(".submenu").stop().slideUp(200);
+    });
+    
+});
